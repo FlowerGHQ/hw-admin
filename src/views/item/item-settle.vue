41fdea613fd7e7e941b0dfbf631392beea46bf75
zhousijia <770187345@qq.com>
2021-12-14 17:32:28 +0800

    采购订单 结算

--- src/views/item/item-settle.vue
+++ src/views/item/item-settle.vue
@@ -1,5 +1,20 @@
 <template>
 <div id="ItemSettle" class="list-container">
+    <div class="title-area">结算</div>
+    <div class="config-list">
+        <div class="config-item">
+            <div class="config-title">1.配送选项</div>
+            <div class="config-content edit-mode" v-if="editMode">
+                
+            </div>
+            <div class="config-content select-mode" v-else></div>
+        </div>
+        <!-- <div class="config-item">
+            <div class="config-title">2.付款</div>
+            <div class="config-content"></div>
+        </div> -->
+    </div>
+    <div class="item-list"></div>
 </div>
 </template>
 
@@ -14,6 +29,8 @@ export default {
             loginType: Core.Data.getLoginType(),
             // 加载
             loading: false,
+
+            editMode: false,
         };
     },
     watch: {},
