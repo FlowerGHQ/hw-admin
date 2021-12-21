5260da6d564e146c145991df17f1f23eee09e785
xuwei <506981148@qq.com>
2021-12-21 17:46:22 +0800

    添加合计

--- src/views/repair/repair-detail.vue
+++ src/views/repair/repair-detail.vue
@@ -3,7 +3,8 @@
     <div class="list-container">
         <div class="title-container">
             <div class="title-area">工单详情</div>
-            <div class="btns-area" v-if="detail.org_type == OrgType">
+<!--            <div class="btns-area" v-if="detail.org_type == OrgType">-->
+            <div class="btns-area">
                 <a-button type="primary" @click="handleSecondDoor()" v-if="detail.status == STATUS.WAIT_CHECK || detail.status == STATUS.WAIT_DETECTION || detail.status == STATUS.WAIT_REPAIR" ><i class="icon i_edit"/>二次维修</a-button>
                 <template v-if="detail.account_id == User.id || $auth('MANMGE')">
                     <a-button type="primary" @click="handleTransfer()"    v-if="detail.status == STATUS.WAIT_CHECK"><i class="icon i_check_c"/>转单</a-button>
@@ -69,7 +70,7 @@
         -->
         <div class="form-container">
             <CheckFault :id='id' :detail='detail' @submit="getRepairDetail" ref="CheckFault"  v-if="detail.status == STATUS.WAIT_DETECTION"/>
-            <CheckResult :id='id' :detail='detail' :faultList="faultList" :failList="failList" :exchangeList="exchangeList" ref="CheckResult" v-if="detail.status != STATUS.WAIT_DETECTION && detail.status != STATUS.WAIT_CHECK" />
+            <CheckResult :id='id' :detail='detail' :faultList="faultList" :failList="failList" :exchangeList="exchangeList" :failTotle="failTotle" :exchangeTotle="exchangeTotle"  ref="CheckResult" v-if="detail.status != STATUS.WAIT_DETECTION && detail.status != STATUS.WAIT_CHECK" />
             <RepairInfo :id='id' :detail='detail' />
             <ActionLog :id='id' :detail='detail' />
         </div>
@@ -181,6 +182,8 @@ export default {
             storeList: [],
             faultList: [],
             failList: [],
+            failTotle: 0,
+            exchangeTotle: 0,
             exchangeList: [],
 
 
@@ -377,20 +380,27 @@ export default {
                 repair_order_id: this.id
             }).then(res => {
                 let failList = []
+                let failTotle = 0
                 let exchangeList = []
+                let exchangeTotle = 0
                 res.list.forEach(it => {
                     it.item_name = it.item.name
                     if (it.type == Core.Const.REPAIR_ITEM.TYPE.ADD) {
                         failList.push(it)
+                        failTotle += it.amount * it.price
                     }
                     if (it.type == Core.Const.REPAIR_ITEM.TYPE.REPLACE) {
                         exchangeList.push(it)
+                        exchangeTotle += it.amount * it.price
                     }
                 })
                 console.log('this.failList:', this.failList)
                 console.log('this.exchangeList:', this.exchangeList)
                 this.failList = failList
+                this.failTotle = failTotle
                 this.exchangeList = exchangeList
+                this.exchangeTotle = exchangeTotle
+                console.log('this.exchangeTotle:', this.exchangeTotle)
             })
         },
     }
