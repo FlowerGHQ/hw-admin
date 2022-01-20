7a63a503d6858e498317736f7060ec669e1fe181
yzy <tang20000101>
2022-01-20 11:07:02 +0800

    维修单 采购单 上传附件去除权限

--- src/views/purchase/purchase-order-detail.vue
+++ src/views/purchase/purchase-order-detail.vue
@@ -160,7 +160,7 @@ import Core from '../../core';
 import PurchaseInfo from "./components/PurchaseInfo.vue"
 import WaybillShow from "@/components/popup-btn/WaybillShow.vue"
 import MySteps from "@/components/common/MySteps.vue"
-import AttachmentFile from './components/AttachmentFile.vue';
+import AttachmentFile from '@/components/popup-btn/AttachmentFile.vue';
 
 const PURCHASE = Core.Const.PURCHASE;
 const STATUS = Core.Const.PURCHASE.STATUS;
