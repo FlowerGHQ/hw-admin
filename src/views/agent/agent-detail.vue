75ecf1b758d6787b2180ffba7669f8eb8126254a
姚志宇 <tang20000101>
2022-01-24 14:42:28 +0800

    改bug门店管理

--- src/views/agent/agent-detail.vue
+++ src/views/agent/agent-detail.vue
@@ -60,12 +60,12 @@
                 <PurchaseList :orgId="agent_id" :orgType="ORG_TYPE.AGENT"  v-if="activeKey === 'PurchaseList'"/>
             </a-tab-pane>
             <template v-if="$auth('ADMIN', 'DISTRIBUTOR')">
-            <a-tab-pane key="StoreList" tab="门店管理">
-                <StoreList :agentId="agent_id" :type="USER_TYPE.AGENT" v-if="activeKey === 'StoreList'" @change="getAgentDetail"/>
-            </a-tab-pane>
-            <a-tab-pane key="PricingStructure" tab="商品价格">
-                <PricingStructure :orgId="agent_id" :orgType="USER_TYPE.AGENT" v-if="activeKey === 'PricingStructure'" :can-edit="$auth('DISTRIBUTOR')"/>
-            </a-tab-pane>
+                <a-tab-pane key="StoreList" tab="门店管理">
+                    <StoreList :agentId="agent_id" :type="USER_TYPE.AGENT" v-if="activeKey === 'StoreList'" @change="getAgentDetail"/>
+                </a-tab-pane>
+                <a-tab-pane key="PricingStructure" tab="商品价格">
+                    <PricingStructure :orgId="agent_id" :orgType="USER_TYPE.AGENT" v-if="activeKey === 'PricingStructure'" :can-edit="$auth('DISTRIBUTOR')"/>
+                </a-tab-pane>
             </template>
         </a-tabs>
     </div>
