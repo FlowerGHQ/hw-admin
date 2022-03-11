eda861045192844e7b59502e50df79af17dc1b13
zhousijia <770187345@qq.com>
2022-03-11 11:18:20 +0800

    生产管理

---
+++ src/views/production/material-edit.vue
@@ -0,0 +1,22 @@
+<template>
+<div></div>
+</template>
+
+<script>
+export default {
+    components: {},
+    props: {},
+    data() {
+        return {
+        }
+    },
+    watch: {},
+    computed: {},
+    onLoad(o) {},
+    onShow() {},
+    methods: {},
+}
+</script>
+
+<style lang='less' scoped>
+</style>
