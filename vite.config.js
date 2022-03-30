520eca79e1a3883dc986a7036f89ebaef51e0aa1
lsn <642250063@qq.com>
2022-03-30 11:34:15 +0800

    配置修改

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import * as path from 'path';
 // https://vitejs.dev/config/
 
 export default defineConfig({
+  server: {
+    port: 8889,
+    proxy: {
+    }
+  },
   plugins: [
     vue(),
     ViteComponents({
