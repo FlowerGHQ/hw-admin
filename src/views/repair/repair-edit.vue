bcb463d5b9c7cfa51acea1b75cda6253c4f998a7
xuwei <506981148@qq.com>
2022-10-25 10:09:00 +0800

    修改bug

--- src/views/repair/repair-edit.vue
+++ src/views/repair/repair-edit.vue
@@ -420,19 +420,19 @@ export default {
                 return this.$message.warning(this.$t('def.enter'))
                 return 0
             }
-            if (!form.channel) {
+            if (!form.channel && form.device_type === Core.Const.REPAIR.DEVICE.FINISHED_AUTOMOBILE) {
                 return this.$message.warning(this.$t('def.enter'))
                 return 0
             }
-            if (!form.repair_method) {
+            if (!form.repair_method && form.device_type === Core.Const.REPAIR.DEVICE.FINISHED_AUTOMOBILE) {
                 return this.$message.warning(this.$t('def.enter'))
                 return 0
             }
-            if (!form.vehicle_no) {
+            if (!form.vehicle_no && form.device_type === Core.Const.REPAIR.DEVICE.FINISHED_AUTOMOBILE) {
                 return this.$message.warning(this.$t('def.enter'))
                 return 0
             }
-            if (this.isExist === false) {
+            if (this.isExist === false && form.device_type === Core.Const.REPAIR.DEVICE.FINISHED_AUTOMOBILE) {
                 return this.$message.warning(this.$t('def.enter'))
             }
             if (form.id) {
