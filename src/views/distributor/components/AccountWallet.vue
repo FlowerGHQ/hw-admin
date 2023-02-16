76ae760a188b49f6788cf515d97e8816de7cd017
15779969509 <ten_ann@163.com>
2023-02-16 17:11:46 +0800

    钱包账户中英文

--- src/views/distributor/components/AccountWallet.vue
+++ src/views/distributor/components/AccountWallet.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="account">
         <a-tabs v-model:activeKey="activeKey">
-            <a-tab-pane key="1" tab="账户余额">
+            <a-tab-pane key="1" :tab="$t('d.account_balance')">
                 1
             </a-tab-pane>
-            <a-tab-pane key="2" tab="余额明细">
+            <a-tab-pane key="2" :tab="$t('d.balance_details')">
                 2
             </a-tab-pane>            
         </a-tabs>
