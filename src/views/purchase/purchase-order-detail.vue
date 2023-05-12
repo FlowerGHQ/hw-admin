b412e295f6df5c5e892b542430bbb59c024958fd
yixinyu <498065027@qq.com>
2023-05-12 15:21:34 +0800

    修改bug

--- src/views/purchase/purchase-order-detail.vue
+++ src/views/purchase/purchase-order-detail.vue
@@ -1128,7 +1128,10 @@ export default {
             if (!form.pay_method) {
                 return this.$message.warning(this.$t('p.please_select_payment_method'))
             }
-            if (!form.payment && !form.accountBalance) {
+            if (!form.payment) {
+                return this.$message.warning(this.$t('p.enter_payment'))
+            }
+            if (!form.accountBalance) {
                 return this.$message.warning(this.$t('p.enter_payment'))
             }
 
