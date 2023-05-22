27ffd97466061d4b35f812ed486d17276fa31c88
yixinyu <498065027@qq.com>
2023-05-22 17:47:51 +0800

    修改导出传参

--- src/views/repair/repair-list.vue
+++ src/views/repair/repair-list.vue
@@ -552,7 +552,7 @@ export default {
                 form[key] = form[key] || ''
             }
             let exportUrl = Core.Api.Export.repairExport({
-                form,
+                ...form,
                 language: this.$i18n.locale === 'en' ? 1 : 0
             })
             console.log("handleRepairExport exportUrl", exportUrl)
