7f43ddc153ae5b160778879e297b546c72bce85e
15779969509 <ten_ann@163.com>
2023-05-29 17:00:13 +0800

    修改分销商图片显示不出来问题

--- src/core/api/index.js
+++ src/core/api/index.js
@@ -251,7 +251,7 @@ const apiList = {
 
     },
 	ItemAccessory: { // 商品
-		// save: ['PostJson', 'item-accessory/save'],   // 当选
+		// save: ['PostJson', 'item-accessory/save'],   // 单选
 		save: ['PostJson', 'item-accessory/save-list'],  // 多选
 		list: ['PostJson', 'item-accessory/list'],
 		delete: ['PostJson', 'item-accessory/delete'],
