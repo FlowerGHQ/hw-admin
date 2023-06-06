27774307a7a370645eb004504f4e327cde429745
yixinyu <498065027@qq.com>
2023-06-06 16:40:02 +0800

    配置请求

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -9,19 +9,31 @@
 
 <script>
 import jsapi from '../../core/jsapi';
+import Core from '../../core';
+import axios from 'axios';
 export default {
     name: 'Index',
     data() {
         return {
-
+            token: ''
         };
     },
     mounted() {
-
+        this.token = Core.Data.getToken()
+        console.log('this.token', this.token);
     },
     methods: {
         test() {
-            console.log('测试');
+            // console.log('测试');
+            axios.post('http://horwintest.natapp1.cc/feishu/authorize/jsapi', {}, {
+                headers: {
+                    'token': this.token
+                }
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('返回数据', data);
+                });
         }
     }
 };
