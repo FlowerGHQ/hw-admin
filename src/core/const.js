8313c97d54541dc040a4e8a5e38387f94ba42597
yixinyu <498065027@qq.com>
2023-06-26 14:38:51 +0800

    支付地址编辑功能 加 部分英文翻译

--- src/core/const.js
+++ src/core/const.js
@@ -34,7 +34,7 @@ switch (window.location.hostname) {
 	    URL_POINT = 'http://eos-dev-api.horwincloud.com'  //测试服
         // URL_POINT = 'http://eos-api.horwincloud.com' // 新正式服
         // URL_POINT = 'http://eos-api-release.horwincloud.com' // 预发环境
-        // URL_POINT = 'http://10.0.0.170:8889' // my
+        // URL_POINT = 'http://10.0.0.170:8888' // my
         // URL_POINT = 'http://10.0.0.213:8889' // zwq
         // URL_POINT = 'http://10.0.0.190:8889' // zy
         break;
