1a9b94cfdcb470abfda8b857bab666afec022375
yixinyu <498065027@qq.com>
2023-08-01 16:19:51 +0800

    fix

--- src/views/repair/repair-detail.vue
+++ src/views/repair/repair-detail.vue
@@ -91,6 +91,17 @@
                 </div>
             </div>
         </div>
+        <a-steps :current="1">
+            <a-step>
+            <!-- <span slot="title">Finished</span> -->
+            <template slot="title">
+                Finished
+            </template>
+            <span slot="description">This is a description.</span>
+            </a-step>
+            <a-step title="In Progress" sub-title="Left 00:00:08" description="This is a description." />
+            <a-step title="Waiting" description="This is a description." />
+        </a-steps>
         <MySteps :stepsList='stepsList' :current='currStep' v-if="detail.status != STATUS.CLOSE"/>
         <div class="form-container">
             <CheckFault  :id='id' :detail='detail' :serviceType='detail.service_type' @submit="getRepairDetail" v-if="detail.status == STATUS.WAIT_DETECTION && sameOrg" ref="CheckFault"/>
