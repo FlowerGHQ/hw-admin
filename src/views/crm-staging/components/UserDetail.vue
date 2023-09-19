f6bb4aa0fbaf7b9d0b5d200995690ee559c9b0a3
yixinyu <498065027@qq.com>
2023-09-19 09:19:03 +0800

    add panel

---
+++ src/views/crm-staging/components/UserDetail.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="user-detail">
+        <div class="user-panel">
+            <img class="avatar" src="../images/avatar.png" alt="">
+        </div>
+        <div class="user-info-wrap">
+
+        </div>
+    </div>
+</template>
+    
+<script>
+import Core from '../../../core';
+export default {
+    name: 'UserDetail',
+    components: {},
+    props: {},
+    data() { 
+        return {
+
+        };
+    },
+    watch: {},
+    computed: {
+        lang() {
+            return this.$store.state.lang
+        }
+    },
+    created() {
+    },
+    mounted() {
+    },
+    methods: {
+
+    }
+};
+</script>
+    
+<style scoped lang="less">
+#user-detail {
+    width: 838px;
+    height: 258px;
+    padding: 20px;
+    box-sizing: border-box;
+    background:#FFF;
+    .user-panel {
+        width: 100%;
+        display: flex;
+        .avatar {
+            width: 80px;
+            height: 80px;
+            border-radius: 50%;
+        }
+    }
+}
+</style>
+    
--- src/views/crm-staging/staging.vue
+++ src/views/crm-staging/staging.vue
@@ -1,14 +1,20 @@
 <template>
     <div id="Staging">
         工作台
+        <UserDetail />
     </div>
 </template>
 
 <script>
 import Core from '../../core';
+import UserDetail from './components/UserDetail.vue';
 export default {
     name: 'Staging',
+    components: {
+        UserDetail,
+    },
     props: {},
+
     data() {
         return {
         };
@@ -23,5 +29,4 @@ export default {
 };
 </script>
 
-<style lang="less" scoped>
-</style>
+<style lang="less" scoped></style>
