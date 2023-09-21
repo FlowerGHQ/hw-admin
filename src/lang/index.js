cf764de964c6f7695a99ecb9bbe8402f178a009f
15779969509 <ten_ann@163.com>
2023-09-21 10:55:01 +0800

    删除log

--- src/lang/index.js
+++ src/lang/index.js
@@ -4,7 +4,7 @@ const messages = {
   zh: {}
 }
 const models = import.meta.globEager(`@/lang/**/*.js`)
-console.log("输出的丁峡谷", models);
+
 Object.keys(models).forEach(key => {
   // 以文件名作为语言包的命名空间
   const namespace = key.split('/')[1]
