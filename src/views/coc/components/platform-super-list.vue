cbf76034eabf6876afd1add2cf3a0b9741ba9503
dzy <dzyperson@163.com>
2023-09-22 10:26:15 +0800

    fix:提取modal个性化的操作

--- src/views/coc/components/platform-super-list.vue
+++ src/views/coc/components/platform-super-list.vue
@@ -82,53 +82,237 @@
 			</div>
 		</div>
 		<EditModal
-			:visible="visible"
 			:ModalTitle="ModalTitle"
-			:openType="openType"
 			v-model:visible="visible"
-			v-model:form="form"
-			:row-id="rowId"
-			@getTableData="getTableData"
-		/>
+			:formdisabled="formdisabled"
+			@handleOk="handleModalOk"
+		>
+			<!-- 内容form表单 -->
+			<a-form
+				:form="form"
+				:layout="formLayout"
+				:label-col="labelCol"
+				:wrapper-col="wrapperCol"
+			>
+				<a-form-item :label="$t('coc.coc_lable_name')" :label-col="labelCol">
+					<a-input
+						v-model:value="form.name"
+						:disabled="formdisabled"
+						:placeholder="$t('coc.coc_placeholder_name')"
+					/>
+				</a-form-item>
+				<a-form-item :label="$t('coc.coc_lable_version')" :label-col="labelCol">
+					<a-input
+						v-model:value="form.version_number"
+						:disabled="formdisabled"
+						:placeholder="$t('coc.coc_placeholder_version')"
+					/>
+				</a-form-item>
+				<a-form-item
+					:label="$t('coc.coc_lable_validity_date')"
+					:label-col="labelCol"
+				>
+					<a-range-picker
+						:show-time="{ format: 'HH:mm:ss' }"
+						v-model:value="coc_validity_date"
+						:disabled="formdisabled"
+						format="YYYY-MM-DD HH:mm:ss"
+						:placeholder="[$t('coc.coc_start_date'), $t('coc.coc_start_date')]"
+					/>
+				</a-form-item>
+				<a-form-item :label="$t('coc.coc_lable_apply_vehicle')" :label-col="labelCol">
+					<!-- select -->
+					<a-select
+						v-model:value="form.model"
+						mode="multiple"
+						showArrow
+						:disabled="formdisabled"
+						:placeholder="$t('coc.coc_placeholder_apply_vehicle')"
+					>
+						<a-select-option
+							v-for="(item, index) in option"
+							:key="index"
+							:value="item"
+						>
+							{{ item }}
+						</a-select-option>
+					</a-select>
+				</a-form-item>
+				<a-form-item :label="$t('coc.coc_lable_template')" :label-col="labelCol">
+					<!-- 上传组件 正方形带+ -->
+					<a-upload
+						:multiple="true"
+						v-model:file-list="fileList"
+						list-type="picture-card"
+						:disabled="formdisabled"
+						action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+						:show-upload-list="false"
+						:before-upload="beforeUpload"
+						@change="handleChange"
+					>
+						<img v-if="imageUrl" :src="imageUrl" alt="avatar" />
+						<div v-else>
+							<loading-outlined v-if="btnLoading"></loading-outlined>
+							<plus-outlined v-else></plus-outlined>
+							<div class="ant-upload-text">
+								{{ $t("coc.coc_placeholder_upload") }}
+							</div>
+						</div>
+					</a-upload>
+				</a-form-item>
+			</a-form>
+		</EditModal>
 	</div>
 </template>
 
 <script setup>
 import Core from "@/core"
 const { Api, Util } = Core
-const { deleteCocTemplate, getCocTemplateList } = Api.COC
+const {
+	deleteCocTemplate,
+	getCocTemplateList,
+	viewCocTemplate,
+	getCateGoryList,
+	addCocTemplate,
+} = Api.COC
 import { useTable } from "@/hooks/useTable"
 import EditModal from "./template-modal.vue"
-import { ref, getCurrentInstance ,} from "vue"
+import { PlusOutlined, LoadingOutlined } from "@ant-design/icons-vue"
+import { ref, getCurrentInstance,onMounted  } from "vue"
 const { ctx } = getCurrentInstance()
 const { $t, $message } = ctx.$root
+
+// 定义常量
+const rowId = ref(null)
 
 // 传递的props
 const ModalTitle = ref("")
 const visible = ref(false)
 const openType = ref("")
-const rowId = ref("")
 // form表单
-const form = ref({})
+const form = ref({
+	name: "",
+	version_number: "",
+	model: [],
+})
+const fileList = ref([])
+// form布局
+const formLayout = ref("horizontal")
+const labelCol = ref({ style: { width: "100px" } })
+const wrapperCol = ref({ span: 16 })
+const btnLoading = ref(false)
+const coc_validity_date = ref([])
+const option = ref([])
+let formdisabled = ref(false)
+const imageUrl  = ref('')
 
-const { tableData, pageChange, pageSizeChange, getTableData, pagination ,loading} =
-	useTable({ request: getCocTemplateList, searchForm: form })
+const {
+	tableData,
+	pageChange,
+	pageSizeChange,
+	getTableData,
+	pagination,
+	loading,
+} = useTable({ request: getCocTemplateList, searchForm: form })
 
 
+// 获取下拉框数据
+const getCateGory = async () => {
+	const res = await getCateGoryList()
+	option.value = res
+}
+// 上传组件
+const beforeUpload = (file) => {
+	console.log("beforeUpload: ", file)
+}
 
-// 将请求promise传给useTable
+const handleChange = (info) => {
+	console.log("handleChange: ", info)
+}
+
+onMounted(() => {
+	getCateGory()
+	getTableData()
+})
+
+// 重置数据
+const resetData = () => {
+	form.value = {
+		name: "",
+		version_number: "",
+		model: [],
+	}
+	fileList.value = []
+	coc_validity_date.value = []
+}
+// 获取详情
+const getDetail = async (id) => {
+	const res = await viewCocTemplate({ id })
+	form.value = res
+	form.value.model =res.model? res.model.split(","):[]
+	// antdesign的日期组件需要传入moment对象
+	// 时间戳转moment对象 案例：1655366541 => 2022-02-15 10:42:21
+	// moment对象转时间戳 案例：moment("2022-02-15 10:42:21").valueOf() => 1655366541000
+	// moment对象转时间戳 案例：moment("2022-02-15 10:42:21").unix() => 1655366541
+	// 时间戳转moment对象 案例：moment.unix(1655366541) => 2022-02-15 10:42:21
+	if (res.effective_start_time && res.effective_end_time) {
+		coc_validity_date.value = [
+			Util.momentFilter(res.effective_start_time),
+			Util.momentFilter(res.effective_end_time),
+		]
+	} else {
+		coc_validity_date.value = []
+	}
+}
+// 添加模板
+const addTemplate = async (form) => {
+	addCocTemplate(form)
+}
+
+// 点击不同的按钮
 const handleModal = (open_type, item = {}) => {
 	if (open_type === "add") {
 		ModalTitle.value = $t("coc.coc_modal_add_title")
+		resetData()
+		formdisabled.value = false
 	} else if (open_type === "detail") {
 		ModalTitle.value = $t("coc.coc_modal_view_title")
+		// 获取详情
+		getDetail(item.id)
+		formdisabled.value = true
 	} else if (open_type === "edit") {
 		ModalTitle.value = $t("coc.coc_modal_edit_title")
+		getDetail(item.id)
+		formdisabled.value = false
 	}
 	openType.value = open_type
 	rowId.value = item.id || null
 	visible.value = true
 }
+// handleModalOk 点击确定按钮
+const handleModalOk = (value) => {
+	if (value === 'edit') {
+		// 处理时间
+		if (coc_validity_date.value.length > 0) {
+			form.value.effective_start_time = coc_validity_date.value[0].unix()
+			form.value.effective_end_time = coc_validity_date.value[1].unix()
+		}
+		if (form.update_time) { 
+			delete form.update_time
+		}
+		// 深拷贝
+		let params = Util.deepCopy(form.value)
+		params.model =params.model.length>0? params.model.join(","):''
+		addCocTemplate(params).then(() => {
+			$message.success($t("coc.coc_add_success"))
+			// 关闭弹窗
+			// 刷新列表
+			getTableData()
+		})
+	}
+	visible.value = false
+}
+// 删除按钮
 const handleDelete = (item) => {
 	deleteCocTemplate({ id: item.id }).then(() => {
 		$message.success($t("coc.coc_delete_success"))
@@ -166,4 +350,14 @@ const tableColumns = [
 ]
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+:deep(.ant-form-item-label) {
+	label {
+		display: inline-flex;
+		white-space: normal;
+		height: 100%;
+		margin-right: 8px;
+		// 与右侧内容对齐
+		align-items: center;
+	}
+}</style>
--- src/views/coc/components/template-modal.vue
+++ src/views/coc/components/template-modal.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Author: douzhiyuan
+ * @Date: 2023-09-20 09:10:42
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2023-09-22 10:25:54
+ * @Description: 请填写简介
+-->
 <template>
 	<div class="tamplate-modal">
 		<a-modal v-model:visible="visible" @ok="handleOk" @cancel="handleCancel">
@@ -13,97 +20,14 @@
 					{{ $t("coc.coc_btn_comfirm") }}
 				</a-button>
 			</template>
-			<!-- 内容form表单 -->
-			<a-form
-				:form="form"
-				:layout="formLayout"
-				:label-col="labelCol"
-				:wrapper-col="wrapperCol"
-			>
-				<a-form-item :label="$t('coc.coc_lable_name')">
-					<a-input
-						v-model:value="form.name"
-						:disabled="formdisabled"
-						:placeholder="$t('coc.coc_placeholder_name')"
-					/>
-				</a-form-item>
-				<a-form-item :label="$t('coc.coc_lable_version')">
-					<a-input
-						v-model:value="form.version_number"
-						:disabled="formdisabled"
-						:placeholder="$t('coc.coc_placeholder_version')"
-					/>
-				</a-form-item>
-				<a-form-item
-					:label="$t('coc.coc_lable_validity_date')"
-					:label-col="labelCol"
-				>
-					<a-range-picker
-						:show-time="{ format: 'HH:mm:ss' }"
-						v-model:value="coc_validity_date"
-						:disabled="formdisabled"
-						format="YYYY-MM-DD HH:mm:ss"
-						:placeholder="[$t('coc.coc_start_date'), $t('coc.coc_start_date')]"
-					/>
-				</a-form-item>
-				<a-form-item :label="$t('coc.coc_lable_apply_vehicle')">
-					<!-- select -->
-					<a-select
-						v-model:value="form.model"
-						mode="multiple"
-						showArrow
-						:disabled="formdisabled"
-						:placeholder="$t('coc.coc_placeholder_apply_vehicle')"
-					>
-						<a-select-option
-							v-for="(item, index) in option"
-							:key="index"
-							:value="item"
-						>
-							{{ item }}
-						</a-select-option>
-					</a-select>
-				</a-form-item>
-				<a-form-item :label="$t('coc.coc_lable_template')">
-					<!-- 上传组件 正方形带+ -->
-					<a-upload
-						:multiple="true"
-						v-model:file-list="fileList"
-						list-type="picture-card"
-						:disabled="formdisabled"
-						action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-						:show-upload-list="false"
-						:before-upload="beforeUpload"
-						@change="handleChange"
-					>
-						<img v-if="imageUrl" :src="imageUrl" alt="avatar" />
-						<div v-else>
-							<loading-outlined v-if="loading"></loading-outlined>
-							<plus-outlined v-else></plus-outlined>
-							<div class="ant-upload-text">
-								{{ $t("coc.coc_placeholder_upload") }}
-							</div>
-						</div>
-					</a-upload>
-				</a-form-item>
-			</a-form>
+			<slot></slot>
 		</a-modal>
 	</div>
 </template>
 <script setup>
-import { ref, getCurrentInstance, onMounted, toRefs, watch ,useAttrs } from "vue"
-import { PlusOutlined, LoadingOutlined } from "@ant-design/icons-vue"
+import {  getCurrentInstance ,useAttrs,defineEmits } from "vue"
 const { ctx } = getCurrentInstance()
-const { $t, $message } = ctx.$root
-import Core from "@/core"
-const { Api, Util } = Core
-// 查询接口
-const { viewCocTemplate, getCateGoryList, addCocTemplate } = Api.COC
-// 穿透给父元素的方法
-const attrs = useAttrs()
-const { onGetTableData } = attrs
-console.log("attrs: ", onGetTableData)
-
+const { $t } = ctx.$root
 const props = defineProps({
 	visible: {
 		type: Boolean,
@@ -113,155 +37,28 @@ const props = defineProps({
 		type: String,
 		default: "",
 	},
-	openType: {
-		type: String,
-		default: "",
+	formdisabled: {
+		type: Boolean,
+		default: false,
 	},
-	rowId: {
-		type: Number,
-		default: null,
-	}
 })
-const $emit = defineEmits(["update:visible", "update:form"])
-// form表单
-const form = ref({
-	name: "",
-	version_number: "",
-	model: [],
-})
-const coc_validity_date = ref([])
-const option = ref([])
-let formdisabled = ref(false)
-// form布局
-const formLayout = ref("horizontal")
-const labelCol = ref({ style: { width: "100px" } })
-const wrapperCol = ref({ span: 16 })
-// 上传组件
-const fileList = ref([])
-const loading = ref(false)
-const imageUrl = ref("")
-
-onMounted(() => {
-	getCateGory()
-})
-
-watch(
-	[() => props.visible, () => props.openType, () => props.rowId],
-	(newVal, oldVal) => {
-		if (newVal[0]) {
-			switch (newVal[1]) {
-				case "add":
-					formdisabled.value = false
-					resetData()
-					break
-				case "detail":
-					formdisabled.value = true
-					getDetail(newVal[2])
-					break
-				case "edit":
-					formdisabled.value = false
-					getDetail(newVal[2])
-					break
-			}
-		}
-	},
-	{
-		deep: true,
-	}
-)
-
-const resetData = () => {
-	form.value = {
-		name: "",
-		version_number: "",
-		model: [],
-	}
-	fileList.value = []
-	coc_validity_date.value = []
-}
-
-const getDetail = async (id) => {
-	const res = await viewCocTemplate({ id })
-	resetData()
-	form.value = res
-	form.value.model = res.model.split(",")
-	// antdesign的日期组件需要传入moment对象
-	// 时间戳转moment对象 案例：1655366541 => 2022-02-15 10:42:21
-	// moment对象转时间戳 案例：moment("2022-02-15 10:42:21").valueOf() => 1655366541000
-	// moment对象转时间戳 案例：moment("2022-02-15 10:42:21").unix() => 1655366541
-	// 时间戳转moment对象 案例：moment.unix(1655366541) => 2022-02-15 10:42:21
-	if (res.effective_start_time && res.effective_end_time) {
-		coc_validity_date.value = [
-			Util.momentFilter(res.effective_start_time),
-			Util.momentFilter(res.effective_end_time),
-		]
-	} else {
-		coc_validity_date.value = []
-	}
-}
-const addTemplate = async (searchForm) => {
-	addCocTemplate(searchForm).then(res => { 
-		if (props.openType === "add") { 
-			$message.success($t("coc.coc_add_success"))
-		} else { 
-			$message.success($t("coc.coc_edit_success"))
-		}
-		$emit("update:visible", false)
-		// 重置表单
-		resetData()
-		// 刷新列表
-		onGetTableData()
-	})
-}
-
-// 获取下拉框数据
-const getCateGory = async () => {
-	const res = await getCateGoryList()
-	option.value = res
-}
-// 上传组件
-const beforeUpload = (file) => {
-	console.log("beforeUpload: ", file)
-}
-
-const handleChange = (info) => {
-	console.log("handleChange: ", info)
-}
-
+const $emit = defineEmits(["update:visible", "handleOk"])
 // modal
 const handleOk = () => {
-	const { openType } = toRefs(props)
-		let reqForm = Util.deepCopy(form.value)
-		reqForm.model = reqForm.model.join(",")
-		reqForm.effective_start_time = coc_validity_date.value[0].unix()
-		reqForm.effective_end_time = coc_validity_date.value[1].unix()
-	if (openType.value === "add") {
-		// 新增
-		addTemplate(reqForm)
-	} else if (openType.value === "edit") {
-		addTemplate(reqForm)
-		// 编辑
-	} else if (openType.value === "detail") {
-		// 查看
-		$emit("update:visible", false)
+	console.log("handleOk",props.formdisabled)
+	if (!props.formdisabled) {
+		$emit('handleOk','edit') //编辑
+	} else { 
+		// 传递修改的字段标识
+		$emit('handleOk','view') //查看
 	}
+
 }
 const handleCancel = () => {
 	$emit("update:visible", false)
-	// 重置表单
-	resetData()
 }
 </script>
 
 <style lang="less" scoped>
-:deep(.ant-form-item-label) {
-	label {
-		display: inline-flex;
-		white-space: normal;
-		height: 100%;
-		margin-right: 8px;
-		// 与右侧内容对齐
-		align-items: center;
-	}
-}
+
 </style>
