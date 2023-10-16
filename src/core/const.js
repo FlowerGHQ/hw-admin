23f88fc6ca93b40a0142616db3c0c23e3cd879ef
yixinyu <498065027@qq.com>
2023-10-16 13:33:25 +0800

    fix: request address 'https'

--- src/core/const.js
+++ src/core/const.js
@@ -13,14 +13,14 @@ let DISABLELOG = false
 
 switch (window.location.hostname) {
     case 'eos.horwincloud.com':
-        URL_POINT = 'http://eos-api.horwincloud.com' // 新正式服
+        URL_POINT = 'https://eos-api.horwincloud.com' // 新正式服
         DISABLELOG = true
         break;
     case 'eos-release.horwincloud.com':
-        URL_POINT = 'http://eos-api-release.horwincloud.com' // 预发环境
+        URL_POINT = 'https://eos-api-release.horwincloud.com' // 预发环境
         break;
     case 'eos-dev.horwincloud.com':
-        URL_POINT = 'http://eos-dev-api.horwincloud.com' // 新测试服
+        URL_POINT = 'https://eos-dev-api.horwincloud.com' // 新测试服
         break;     
     case "10.0.0.213":
         URL_POINT = 'http://10.0.0.213:8889'
@@ -28,16 +28,16 @@ switch (window.location.hostname) {
         // URL_POINT = 'http://eos-api.horwincloud.com' // 正式服
         break;
     case 'eos.hw.innotick.com':
-        URL_POINT = 'http://eos-api.horwincloud.com' // 老正式服
+        URL_POINT = 'https://eos-api.horwincloud.com' // 老正式服
         break;
     case 'eos-dev.hw.innotick.com':
-        URL_POINT = 'http://eos-dev-api.horwincloud.com' // 老测试服
+        URL_POINT = 'https://eos-dev-api.horwincloud.com' // 老测试服
         break;
     case 'eos-release.horwincloud.com':
         URL_POINT = 'http://eos-api-release.horwincloud.com' // 预发环境
         break;
     default:
-        URL_POINT = 'http://eos-dev-api.horwincloud.com'  //测试服
+        URL_POINT = 'https://eos-dev-api.horwincloud.com'  //测试服
         // URL_POINT = 'http://eos-api.horwincloud.com' // 新正式服
         // URL_POINT = 'http://eos-api-release.horwincloud.com' // 预发环境
         // URL_POINT = 'http://10.0.0.170:8889' // my
