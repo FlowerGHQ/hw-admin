3dd9ec590b7d7f8db9b4977d6bdc90af7917f4fd
dzy <dzyperson@163.com>
2023-11-28 14:53:07 +0800

    fix:修改展示图

--- src/views/purchase/components/UpAndDownSwiper.vue
+++ src/views/purchase/components/UpAndDownSwiper.vue
@@ -1,22 +1,9 @@
 <template>
   <div class="banner_container">
-    <!-- <a-carousel arrows dots-class="slick-dots slick-thumb" dotPosition="left" :afterChange="handleAafterChange" v-if="type === 0">
-            <template #prevArrow>
-                <div class="custom-slick-arrow" style="left: 10px; z-index: 1">
-                    <up-outlined />
-                </div>
-            </template>
-            <template #nextArrow>
-                <div class="custom-slick-arrow" style="right: 10px">
-                    <down-outlined />
-                </div>
-            </template>
-        </a-carousel> -->
-
     <div class="img-big">
       <img :src="getImgUrl(imgIndex)" />
     </div>
-    <div class="img-active"  v-if="imgsArr.length>0">
+    <!-- <div class="img-active"  v-if="imgsArr.length>0">
       <div class="left-icon">
         <left-outlined @click="imgPreview(0)" />
       </div>
@@ -33,7 +20,7 @@
       <div class="right-icon">
         <right-outlined @click="imgPreview(1)" />
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -88,46 +75,33 @@ export default {
     imgsArr() {
       let activeObj = {}
       activeObj = this.type === 1 ? this.imgs : this.imgs.find((item) => item.id === this.item_id);
-      console.log("activeObj:", activeObj);
-      // 如果activeObj.logo或者imgs不存在，返回空数组
-      if (!activeObj.logo && !activeObj.imgs) return []
-      let arr = []
-        arr =activeObj.imgs === ''?[]:activeObj.imgs.split(",")
-        // 找到imgs中的logo
-        let logo = activeObj.logo
-        let index = arr.indexOf(logo)
-        // 将logo放到第一位
-        if (index > -1) {
-          arr.splice(index, 1)
-        } 
-        if(logo !== ''&& logo) arr.unshift(logo)
-        return arr
+      return activeObj 
     },
   },
   methods: {
     getImgUrl() {
-      return this.$Util.imageFilter(this.imgsArr?this.imgsArr[this.imgIndex]:[],2);
+      return this.$Util.imageFilter(this.imgsArr.logo,2);
     },
-    imgPreview(type) {
-      if (type === 0) {
-        if (this.imgIndex === 0) {
-          this.imgIndex = this.imgsArr.length - 1;
-        } else {
-          this.imgIndex--;
-        }
-      } else {
-        if (this.imgIndex === this.imgsArr.length - 1) {
-          this.imgIndex = 0;
-        } else {
-          this.imgIndex++;
-        }
-      }
-    //   让当前图片居中
-      this.$refs.imgArea.scrollLeft = this.imgIndex * 70;
-    },
-    previewImg(index) {
-      this.imgIndex = index;
-    },
+    // imgPreview(type) {
+    //   if (type === 0) {
+    //     if (this.imgIndex === 0) {
+    //       this.imgIndex = this.imgsArr.length - 1;
+    //     } else {
+    //       this.imgIndex--;
+    //     }
+    //   } else {
+    //     if (this.imgIndex === this.imgsArr.length - 1) {
+    //       this.imgIndex = 0;
+    //     } else {
+    //       this.imgIndex++;
+    //     }
+    //   }
+    // //   让当前图片居中
+    //   this.$refs.imgArea.scrollLeft = this.imgIndex * 70;
+    // },
+    // previewImg(index) {
+    //   this.imgIndex = index;
+    // },
   },
 };
 </script>
