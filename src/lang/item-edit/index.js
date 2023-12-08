0ee9813b2b4fc262cc40d0a163eefe301c8a5bdd
15779969509 <ten_ann@163.com>
2023-12-08 16:41:52 +0800

    fix: 修改ICON文案

--- src/lang/item-edit/index.js
+++ src/lang/item-edit/index.js
@@ -11,7 +11,7 @@ export default {
 		add_spec: '新增规格',
 		// 图片预览
 		preview: '图片预览',
-		flag_entity_keyword: '有些商品有唯一的ID，有一些没有。比如整车会有唯一ID，但是螺丝没有。出入库时，会根据商品的实例编码进行库存统计。',
+		flag_entity_keyword: '实力编码：有些商品具有唯一标识，例如整车可通过整车码做为区分标识，就属于有实例编码。出入库时可以根据实例编码进行库存统计。',
 		item_number_keyword: '一个系列的商品的编码，相当于一个商品的姓，比如EK3版本：HW3500T-1;',
 		drawing_code_keyword: '图面代号是整车机种开发代号，会影响三码绑定整车码的形成，请仔细填写。',
     },
@@ -20,7 +20,7 @@ export default {
 		add_spec: 'Add Spec',
 		// 图片预览
 		preview: 'Preview',
-		flag_entity_keyword: "Some items have unique ids, some don't. For example, the whole car will have a unique ID, but the screw will not. When entering or exiting the warehouse, inventory statistics are performed according to the instance code of the item.",
+		flag_entity_keyword: "Strength code: some goods have a unique identification, for example, the whole vehicle can be identified by the whole vehicle code, which belongs to the case code. Inventory statistics can be carried out according to the case code when entering and leaving the warehouse.",
 		item_number_keyword: "The code of a series of goods is equivalent to the surname of a product, such as the EK3 version: HW3500T-1;",
 		drawing_code_keyword: "The drawing code is the vehicle type development code, which will affect the formation of the three-code binding vehicle code, please fill in carefully.",
     }
