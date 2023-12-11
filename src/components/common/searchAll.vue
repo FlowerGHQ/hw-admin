dd4522c79549160c08949355e24bb617017c10bd
maoxinya <18345740829@126.com>
2023-12-11 11:09:42 +0800

    fix: 更改组件逻辑、更改格式

--- src/components/common/searchAll.vue
+++ src/components/common/searchAll.vue
@@ -1,49 +1,69 @@
-<template>
-    <div class="search-container">
-        <!-- 
-            例子：
-                <searchAll :options = "searchList" @search = "getSearchFrom" @reset = "handleSearchReset" >
-                    // 时间插槽
-                    <template v-slot:time>
-                        <div  class="item-box">
-                            <div class="key-box">
-                                {{ $t("d.create_time") }}
-                            </div>
-                            <div class="value-box">
-                                <TimeSearch @search="handleOtherSearch" ref="TimeSearch" />
-                            </div>
+
+    <!-- 
+        例子：
+            <searchAll :options = "searchList" @search = "getSearchFrom" @reset = "handleSearchReset" >
+                // 时间插槽
+                <template v-slot:time>
+                    <div  class="item-box">
+                        <div class="key-box">
+                            {{ $t("d.create_time") }}
                         </div>
-                    </template>
-                </searchAll>
-            
-                搜索组件：样式+重置+查询+可slot自定义(slot = extend、slot = time)
+                        <div class="value-box">
+                            <TimeSearch @search="handleOtherSearch" ref="TimeSearch" />
+                        </div>
+                    </div>
+                </template>
+            </searchAll>
+        
+            搜索组件：样式+重置+查询+可slot自定义(slot = extend、slot = time)
 
-                使用方法：slot = deafult
-                    1.父组件prop: options 例如： 
-                            options = [
-                                { type: "input", value: "name",  tabname: 'n.name', },                                           // 名称
-                                { type: "select", value: "type",  tabname: 'n.type', selectMap: this.itemTypeMap },              // 类型
-                                { type: "select-val", value: "source_type",  tabname: 'i.source_type', selectMap: this.SOURCE_TYPE }, // 来源
-                                { type: "input", value: "code", tabname: 'i.code', },                                        // 商品编码
-                                { type: "select", value: "status", tabname: 'i.status', selectMap: this.itemStatusMap },    // 商品状态  
-                            ]
+            使用方法：slot = deafult
+                1.父组件prop: options 例如： 
+                        options = [
+                            { type: "input", value: "name",  tabname: 'n.name', },                                           // 名称
+                            { type: "select", value: "type",  tabname: 'n.type', selectMap: this.itemTypeMap },              // 类型
+                            { type: "select-val", value: "source_type",  tabname: 'i.source_type', selectMap: this.SOURCE_TYPE }, // 来源
+                            { type: "input", value: "code", tabname: 'i.code', },                                        // 商品编码
+                            { type: "select", value: "status", tabname: 'i.status', selectMap: this.itemStatusMap },    // 商品状态  
+                        ]
 
-                            属性使用：    
-                                type： 填写项类型 input、select、select-val
-                                selectMap: 来选择下拉的数组
-                                tabname：填写项名称
-                                value： 需要请求的属性名
+                        属性使用：    
+                            type： 填写项类型 input、select、select-val
+                            selectMap: 来选择下拉的数组
+                            tabname：填写项名称
+                            value： 需要请求的属性名
 
-                    2.父组件接收
+                2.父组件接收
 
-                        //父组件中接收子组件传的data
-                        getSearchFrom(data) {
-                            for(let key in data){
-                                this.searchForm[key] = data[key];
-                            }
-                            this.handleSearch();
-                        },
-        -->
+                    //父组件中接收子组件传的data
+                    getSearchFrom(data) {
+                        for(let key in data){
+                            this.searchForm[key] = data[key];
+                        }
+                        this.handleSearch();
+                    },
+
+
+                    select: 
+
+                            STATUS_LIST: [
+                                { zh: '全部', en: 'All', value: '1' },
+                                { zh: '上架中', en: 'Active', value: '0' },
+                                { zh: '已下架', en: 'Inactive', value: '-1' },
+                            ],
+                    select-val:
+                    
+                            SOURCE_TYPE: {
+                                '1': { id: 1, value: 'EOS' },
+                                '2': { id: 2, value: 'ERP' }
+                            },
+
+
+
+    -->
+<template>
+    <div class="search-container search-container-new">
+
         <a-row class="search-area">
             <slot name="deafult">
                 <template v-for="(item, index) in options" :key="index">
@@ -70,7 +90,7 @@
                                     <a-select-option
                                         v-for="(val, key) in item.selectMap"
                                         :key="key"
-                                        :value="key"
+                                        :value="val.value"
                                         >{{ val[$i18n.locale] }}</a-select-option
                                     >
                                 </a-select>
@@ -81,15 +101,16 @@
                                 {{ $t(item.tabname) }}
                             </div>
                             <div class="value-box">
-                                <a-select
-                                    v-model:value="searchForm[item.value]"
-                                    :placeholder="$t('def.select')">
-                                    <a-select-option
-                                        v-for="(val, key) in item.selectMap"
-                                        :key="key"
-                                        :value="val.id"
-                                        >{{ val.value }}</a-select-option
-                                    >
+                                    <a-select
+                                        v-model:value="searchForm[item.value]"
+                                        :placeholder="$t('def.select')">
+                                        <a-select-option
+                                            v-for="(val, key) in item.selectMap"
+                                            :key="key"
+                                            :value="val.id"
+                                        >
+                                            {{ val.value }}
+                                        </a-select-option>
                                 </a-select>
                             </div>
                         </div>
@@ -97,33 +118,28 @@
                 </template>
             </slot>
             <a-col :xs="24" :sm="24" :xl="16" :xxl="12" class="search-box" v-if="show">
-                <slot name="time">
-                </slot>
+                <slot name="time"></slot>
             </a-col>
-            <a-col :xs="24" :sm="24" :xl="8" :xxl="6" class="search-box" v-if="show">
-                <slot name="extend">
-                </slot>
-            </a-col>
+            <slot name="extend"></slot>
         </a-row>
         <div class="btn-area-box">
-            <a-button @click="handleSearch" type="primary">{{
-            $t("def.search")
-            }}</a-button>
+            <a-button @click="handleSearch" type="primary">
+                {{  $t("def.search") }}
+            </a-button>
             <a-button @click="handleSearchReset">{{ $t("def.reset") }}</a-button>
             
             <a-button type="link" @click="moreSearch">
-            {{ show ? $t("def.stow") : $t("def.unfold") }}
-            <i
-            class="icon i_xialajiantouxiao"
-            style="margin-left: 5px"
-            v-if="!show"
-            ></i>
-            <i
-            class="icon i_shouqijiantouxiao"
-            style="margin-left: 5px"
-            v-else
-            ></i>
-            
+                {{ show ? $t("def.stow") : $t("def.unfold") }}
+                <i
+                    class="icon i_xialajiantouxiao"
+                    style="margin-left: 5px"
+                    v-if="!show"
+                ></i>
+                <i
+                    class="icon i_shouqijiantouxiao"
+                    style="margin-left: 5px"
+                    v-else
+                ></i>
             </a-button>
         </div>
     </div>
@@ -163,7 +179,7 @@ export default {
             },
                 deep: true,
                 immediate: true
-            }
+        }
     },
     mounted() {
     },
@@ -190,47 +206,6 @@ export default {
     },
 }
 </script>
-<Style lang="less" scoped>
+<style lang="less" scoped>
 
-    .item-box {
-        display: flex;
-        margin: 0 20px 20px 0;
-
-        .key-box {
-            padding: 0px 10px;
-            box-sizing: border-box;
-            display: flex;
-            align-items: center;
-
-            color:  #1D2129;
-            font-size: 14px;
-            font-weight: 400;
-            border: 1px solid #E2E2EA;
-            border-radius: 4px 0px 0px 4px ;
-            border-right: none;
-            background-color: #FFFFFF;
-
-        }
-        
-        .value-box {
-
-            flex: 1;
-            overflow: hidden;
-
-            >.ant-input,
-            >.ant-select,
-            >.ant-select-selector,
-            >.ant-picker,
-            >.ant-cascader-picker,
-            >.ant-select-single,
-            >.ant-select-show-arrow {
-                width: 100%;
-                border-radius: 0px 4px 4px 0px !important;
-            }
-            .ant-select:not(.ant-select-customize-input) .ant-select-selector{
-                border-radius: 0px 4px 4px 0px !important;
-            }
-            
-        }
-    }
-</Style>
+</style>
