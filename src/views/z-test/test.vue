25be662899b8d30e62b40b1b4c5a0ce96c7790d9
15779969509 <ten_ann@163.com>
2023-12-12 20:38:54 +0800

    fix: BOM两条线demo完成

--- src/views/z-test/test.vue
+++ src/views/z-test/test.vue
@@ -1,36 +1,47 @@
 <template>
     <div class="test">
-        <canvas ref="exploreCanvas"></canvas>
-        <div 
-            v-for="(item, index) in pointerList" :key="index"
-            class="pointer-start" 
-            :style="{'left': `${item?.start?.x}px`, 'top': `${item?.start?.y }px`}"
-        >
-        </div>
-        <div 
-            v-for="(item, index) in pointerList" :key="index"
-            class="pointer-end"
-            :style="{'left': `${item?.end?.x}px`, 'top': `${item?.end?.y }px`}"
-        >
-        
+        <div class="point-contain" @mouseup="mouseupHandler" @mousemove="mousemoveHandler">
+            <img ref="exploreImg" src="../../assets//images/vehicle.png" alt="">
+            <canvas class="explore-canvas" ref="exploreCanvas"></canvas>
+
+            <template v-for="(item, itemIndex) in pointerList">            
+                <div      
+                    v-for="(itemStart, itemStartIndex) in item.start" :key="index"     
+                    class="pointer-start" 
+                    :style="{'left': `${itemStart?.x}px`, 'top': `${itemStart?.y }px`}"
+                    @mousedown="pointMousedown(itemIndex, 'start', itemStartIndex)"
+                    @mousemove.stop=""
+                >
+                </div>
+            </template>
+            <div 
+                v-for="(item, index) in pointerList" :key="index"
+                class="pointer-end"
+                :style="{'left': `${item?.end?.x}px`, 'top': `${item?.end?.y }px`}"
+                @mousedown="pointMousedown(index, 'end')"
+                @mousemove.stop=""
+            >
+                {{item.index || 0}}
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
 import { onMounted, ref, getCurrentInstance } from 'vue';
-import imgs  from '../../assets/images/vehicle.png'
-let ctx = null // canvas 2d实例
+
+var ctx = null // canvas 2d实例
 const { proxy } = getCurrentInstance();
 
 
 
-const exploreCanvas = ref(null)
+const exploreCanvas = ref(null) // canvas ref
+const exploreImg = ref(null) // 照片 ref 之所以不直接画在canvas中是为了后面移动要清除整个画布
 
 const pointerList = ref([
     {
-        end_point:"{\"x\":423,\"y\":310}",
-        start_point:"{\"x\":416,\"y\":153}",
+        end_point:"{\"x\":423,\"y\":310}",  // 结束点只有一个
+        start_point:"[{\"x\":416,\"y\":153}, {\"x\":316,\"y\":253}]", // 开始点有多个
         create_time:1702363893,
         id:1335,
         index:1,        
@@ -43,7 +54,7 @@ const pointerList = ref([
     },
     {
         end_point:"{\"x\":74,\"y\":67}",
-        start_point: "{\"x\":137,\"y\":223}",
+        start_point: "[{\"x\":137,\"y\":223},{\"x\":237,\"y\":323}]",
         create_time:1702363893,
         id:1335,
         index:1,        
@@ -56,34 +67,76 @@ const pointerList = ref([
     }
 ]) // 点位列表
 
+// 移动点操作
+const moveParams = ref({
+    isMove: false, // 是否可以移动了
+    moveIndex: undefined, // 移动第几个索引
+    moveType: undefined, // 移动是移动哪个类型
+    itemStartIndex: undefined,
+})
+
 onMounted(() => {
     pointerList.value = pointerList.value.map(el => {
         return {
             ...el,
-            start: JSON.parse(el.start_point),
+            start: JSON.parse(el.start_point) instanceof Array ? JSON.parse(el.start_point) : [],
             end: JSON.parse(el.end_point),
         }
     })
-    console.log("结果", pointerList.value);
+    // console.log("结果", pointerList.value);
 
-    loadImage()
+    init()
 })
 
 /* methods */
-// 加载照片
-const loadImage = () => {
-    ctx = exploreCanvas.value.getContext("2d")
+const test = () => {
+    console.log("创建时间");
+}
+// 容器内鼠标移动
+const mousemoveHandler = (e) => {
+    if(!moveParams.value.isMove)  return;
 
-    let img = new Image()
+    if (moveParams.value.moveType === 'start') {
 
-    img.src = imgs
+        pointerList.value[moveParams.value.moveIndex][moveParams.value.moveType][moveParams.value.itemStartIndex].x = e.offsetX;
+        pointerList.value[moveParams.value.moveIndex][moveParams.value.moveType][moveParams.value.itemStartIndex].y = e.offsetY;
+        console.log("结果 start", pointerList.value);
+    } else if (moveParams.value.moveType === 'end') {
 
-    console.dir(img);
-
-    img.onload = function() {
-        setCanvasAttr(img.width, img.height)
-        ctx.drawImage(img, 0, 0)
+        pointerList.value[moveParams.value.moveIndex][moveParams.value.moveType].x = e.offsetX;
+        pointerList.value[moveParams.value.moveIndex][moveParams.value.moveType].y = e.offsetY;
+        console.log("结果 end", pointerList.value);
     }
+    
+    window.requestAnimationFrame(() => {
+        canvasLine(ctx)
+    })
+}
+// 容器内鼠标移动
+const mouseupHandler = () => {
+    console.log("松开");
+    moveParams.value.isMove = false    
+    moveParams.value.moveIndex = undefined    
+    moveParams.value.moveType = undefined    
+    moveParams.value.itemStartIndex = undefined
+}
+// points 鼠标按下
+const pointMousedown = (index, type, itemStartIndex) => {
+    moveParams.value.isMove = true
+    moveParams.value.moveIndex = index
+    moveParams.value.moveType = type
+    moveParams.value.itemStartIndex = itemStartIndex || undefined
+}
+// 初始化
+const init = () => {
+    ctx = exploreCanvas.value.getContext("2d")
+    
+    // 等图片加载完
+    exploreImg.value.onload = () => {
+        setCanvasAttr(exploreImg.value.width, exploreImg.value.height)
+        canvasLine(ctx)
+    }
+    
 }
 // 设置canvas的大小
 const setCanvasAttr = (width, height) => {
@@ -96,46 +149,91 @@ const setCanvasAttr = (width, height) => {
     } else {
         exploreCanvas.value.width = width;
         exploreCanvas.value.height = height;
-    }
-
+    }    
 }
 
-// 初始化画线
+// 画线
 const canvasLine = (ctx) => {
+    clearCanvas(ctx)
+
+    ctx.beginPath();
+    ctx.lineWidth = 1
+    ctx.strokeStyle = '#1890ff';
+    pointerList.value.forEach($1 => {
+        var start = $1.start
+        var end = $1.end
+
+        if (start.length > 0) {
+            start.forEach($2 => {                
+                ctx.moveTo($2.x, $2.y)
+                ctx.lineTo(end.x, end.y)
+                ctx.stroke();
+            })
+        } else {
+            ctx.moveTo(start[0].x, start[0].y)
+            ctx.lineTo(end.x, end.y)
+            ctx.stroke();
+        }
+    })
 
 }
 
 // 清除画布
-// const canvasClear =
+const clearCanvas = (ctx) => {
+    ctx.clearRect(0, 0, exploreCanvas.value.width, exploreCanvas.value.height)
+}
+
+const btn = () => {
+    clearCanvas(ctx)
+}
 </script>
 
 <style lang="less" scoped>
 .test {
     margin: 90px;
-    position: relative;
-    .pointer-start, .pointer-end  {
-        position: absolute;
-        z-index: 10;
-        border-radius: 50px;
-        user-select: none;
-        opacity: 0.6;
-        transition: opacity 0.15s ease;
-        transform: translate(-50%, -50%);
-        cursor: pointer;
-        &:hover {
-            z-index: 20;
-            opacity: 1;
+    
+    .point-contain {
+        
+        display: inline-block;
+        position: relative;
+        // max-width: 800px;
+        // max-height: 800px;
+        // min-height: 100px;
+
+        .explore-canvas {
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
         }
-    }
-    .pointer-start {
-        width: 8px;
-        height: 8px;
-        background-color: blue;
-    }
-    .pointer-end {
-        width: 20px;
-        height: 20px;
-        background-color: red;
+        .pointer-start, .pointer-end  {
+            position: absolute;
+            z-index: 10;
+            border-radius: 50px;
+            user-select: none;
+            opacity: 0.6;
+            transition: opacity 0.15s ease;
+            transform: translate(-50%, -50%);
+            cursor: pointer;
+            &:hover {
+                z-index: 20;
+                opacity: 1;
+            }
+        }
+        .pointer-start {
+            width: 8px;
+            height: 8px;
+            background-color: @BG_LP;
+        }
+        .pointer-end {
+            width: 20px;
+            height: 20px;
+            line-height: 20px;
+            text-align: center;
+            background-color: @BG_LP;
+            color: #fff;
+        }
     }
 }
 </style>
