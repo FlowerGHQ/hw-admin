a48ec8f2f79d2137d067afc2aadd748ed5c22267
15779969509 <ten_ann@163.com>
2023-12-12 14:37:35 +0800

    fix: 修改表格样式

--- src/lang/item-bom/index.js
+++ src/lang/item-bom/index.js
@@ -2,8 +2,26 @@ export default {
 
     zh: {
         title: "BOM管理",
+        product_name: "商品名称",
+        commodity_code: "商品编码",
+        manufacturer_name: "厂商名称",
+        version_number: "版本号",
+        dosage: "用量",
+        commodity_picture: "商品图片",
+        sales_area: "销售区域",
+        create_time: "创建时间",
+        remark: "备注",
     },
     en: {
         title: "BOM Management",
+        product_name: "Product Name",
+        commodity_code: "Commodity Code",
+        manufacturer_name: "Manufacturer Name",
+        version_number: "Version Number",
+        dosage: "Dosage",
+        commodity_picture: "Commodity Picture",
+        sales_area: "Sales Area",
+        create_time: "Creation Time",
+        remark: "Remark",
     }
 }
--- src/views/item/components/item-bom/Fittings.vue
+++ src/views/item/components/item-bom/Fittings.vue
@@ -10,67 +10,139 @@
         >   
             <!-- :pagination="channelPagination"
             @change="handleTableChange" -->
-            <template #bodyCell="{ column, text, record }">
-                <!-- <span v-if="column.name === ''"></span> -->
+            <template #bodyCell="{ column, text, record }">            
+                <span v-if="column.key === 'name'/*商品名称*/">
+                    <a-tooltip>
+                        <template #title>{{ text }}</template>
+                        <div 
+                            class="one-spils cursor" 
+                            :style="{ width: text?.length > 6 ? 7 * 12 + 'px' : '' }"
+                        >
+                            {{ text }}
+                        </div>
+                    </a-tooltip>
+                </span>
+                <span v-if="column.key === 'manufacturer_name'/*厂商名称*/">
+                    <a-tooltip>
+                        <template #title>{{ text }}</template>
+                        <div 
+                            class="one-spils cursor" 
+                            :style="{ width: text?.length > 4 ? 5 * 12 + 'px' : '' }"
+                        >
+                            {{ text }}
+                        </div>
+                    </a-tooltip>
+                </span>
+                <span v-if="column.key === 'sales_area'/*销售区域*/">
+                    <a-tooltip>
+                        <template #title>{{ text }}</template>
+                        <div 
+                            class="one-spils cursor" 
+                            :style="{ width: text?.length > 5 ? 6 * 12 + 'px' : '' }"
+                        >
+                            {{ text }}
+                        </div>
+                    </a-tooltip>
+                </span>
+                <span v-if="column.key === 'remark'/*备注*/">
+                    <a-tooltip>
+                        <template #title>{{ text }}</template>
+                        <div class="one-spils set-width cursor">
+                            {{ text }}
+                        </div>
+                    </a-tooltip>
+                </span>
             </template>
         </a-table>
     </div>
 </template>
 
 <script setup>
-import { onMounted, ref, getCurrentInstance } from 'vue';
+import { onMounted, ref, getCurrentInstance, computed } from 'vue';
 import Core from "@/core";
 
 const { proxy } = getCurrentInstance();
 
-const tableColumns = ref([
-    { 
-        title: "商品名称", 
-        dataIndex: "name", 
-        key: "detail"
+
+const tableColumns = computed(() => {
+    const result = [
+        { 
+            // 商品名称
+            title: proxy.$t('item-bom.product_name'), 
+            dataIndex: "name", 
+            key: "name"
+        },
+        { 
+            // 商品编码
+            title: proxy.$t('item-bom.commodity_code'), 
+            dataIndex: "name", 
+            key: "detail"
+        },
+        { 
+            // 厂商名称
+            title: proxy.$t('item-bom.manufacturer_name'), 
+            dataIndex: "manufacturer_name", 
+            key: "manufacturer_name"
+        },
+        { 
+            // 版本号
+            title: proxy.$t('item-bom.version_number'), 
+            dataIndex: "name", 
+            key: "detail"
+        },
+        { 
+            // 用量
+            title: proxy.$t('item-bom.dosage'), 
+            dataIndex: "name", 
+            key: "detail"
+        },
+        { 
+            // 商品图片
+            title: proxy.$t('item-bom.commodity_picture'), 
+            dataIndex: "name", 
+            key: "detail"
+        },
+        { 
+            // 销售区域
+            title: proxy.$t('item-bom.sales_area'), 
+            dataIndex: "sales_area", 
+            key: "sales_area"
+        },
+        { 
+            // 创建时间
+            title: proxy.$t('item-bom.create_time'), 
+            dataIndex: "create_time", 
+            key: "detail"
+        },
+        {
+            // 备注
+            title: proxy.$t('item-bom.remark'), 
+            dataIndex: "remark",
+            key: "remark",
+        },
+    ]
+    return result
+})
+const tableData = ref([
+    {
+        name: "你好啊你好啊你好啊",
+        manufacturer_name: "你好啊你好啊你好啊",
+        sales_area: "你好啊你好啊你好啊",
+        remark: "测试的东西是什么啊送达的傻大姐撒肯定撒看的艰苦撒旦就卡死进度加快撒可见度刷卡机的空间的健康撒可见度",
     },
-    { 
-        title: "商品编码", 
-        dataIndex: "name", 
-        key: "detail"
+    {
+        name: "你好啊世界经济发",
+        manufacturer_name: "你好啊世界经济发",
+        sales_area: "你好啊世界经济发",
+        remark: "ss",
     },
-    { 
-        title: "厂商名称", 
-        dataIndex: "name", 
-        key: "detail"
-    },
-    { 
-        title: "版本号", 
-        dataIndex: "name", 
-        key: "detail"
-    },
-    { 
-        title: "用量", 
-        dataIndex: "name", 
-        key: "detail"
-    },
-    { 
-        title: "商品图片", 
-        dataIndex: "name", 
-        key: "detail"
-    },
-    { 
-        title: "销售区域", 
-        dataIndex: "name", 
-        key: "detail"
-    },
-    { 
-        title: "创建时间", 
-        dataIndex: "create_time", 
-        key: "detail"
-    },
-    { 
-        title: "备注", 
-        dataIndex: "remark",
-        key: "remark"
+    {
+        name: "你好啊sss",
+        manufacturer_name: "你好啊sss",
+        sales_area: "你好啊sss",
+        remark: "ss",
     },
 ])
-const tableData = ref([])
 // 分页
 const channelPagination = ref({
     current: 1,
@@ -84,7 +156,7 @@ const channelPagination = ref({
 
 
 onMounted(() => {
-    getTableDataFetch()
+    // getTableDataFetch()
 })
 /* Fetch start*/
 // 获取表格list
@@ -128,5 +200,9 @@ const handleTableChange = (pagination, filters, sorter) => {
 <style lang="less" scoped>
 .fittings {
     width: 100%;
+}
+
+.set-width {
+    width: 100px;
 }
 </style>
