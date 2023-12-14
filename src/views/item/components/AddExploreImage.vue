ed9e34449964649c2b827c6d7202406c1d106d15
yixinyu <498065027@qq.com>
2023-12-14 09:33:08 +0800

    fix：add i18n

--- src/views/item/components/AddExploreImage.vue
+++ src/views/item/components/AddExploreImage.vue
@@ -128,8 +128,6 @@ export default {
                 this.$message.warning(this.$t('n.picture_smaller'));
             }
 
-            // this.loadImage(TEST_IMAGE);
-            // return false;
             return isCanUpType && isLt10M;
         },
         // 上传图片
@@ -141,7 +139,6 @@ export default {
                 this.shortPath = get(fileList,'[0].response.data.filename', null);
                 if(this.shortPath) {
                     this.form.img = this.shortPath;
-                    // this.loadImage(this.detailImageUrl);
                 }
             }
             this.upload.coverList = fileList;
