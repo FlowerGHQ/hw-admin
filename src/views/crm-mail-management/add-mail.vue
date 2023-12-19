3e57c087ddb5f862c619176393dfaef5fba5f9e5
15779969509 <ten_ann@163.com>
2023-12-19 11:31:53 +0800

    fix: 基本架子完成

---
+++ src/views/crm-mail-management/add-mail.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="add-mail edit-container">
+        <div class="title-container">
+            <div class="title-area">你好啊</div>
+        </div>
+
+        <div class="container-block">
+            <a-row class="horizontal-search-row">
+                <a-col class="search-col">
+                    <div class="key">
+                        选择模板
+                    </div>
+                    <div class="value">
+                        
+                    </div>
+                </a-col>
+                <a-col class="search-col required">
+                    <div class="key">
+                        收件人
+                    </div>
+                    <div class="value">
+                        <a-radio v-model:checked="checked">Radio</a-radio>
+                    </div>
+                </a-col>
+                <a-col class="search-col">
+                    <div class="key">
+                        主标题
+                    </div>
+                    <div class="value">
+                        
+                    </div>
+                </a-col>
+                <a-col class="search-col">
+                    <div class="key">
+                        开头称呼
+                    </div>
+                    <div class="value">
+                        
+                    </div>
+                </a-col>
+                <a-col class="search-col">
+                    <div class="key">
+                        正文内容
+                    </div>
+                    <div class="value">
+                        
+                    </div>
+                </a-col>
+                <a-col class="search-col">
+                    <div class="key">
+                        链接
+                    </div>
+                    <div class="value">
+                        
+                    </div>
+                </a-col>
+                <a-col class="search-col">
+                    <div class="key">
+                        海报
+                    </div>
+                    <div class="value">
+                        
+                    </div>
+                </a-col>
+                <a-col class="search-col">
+                    <div class="key">
+                        二维码
+                    </div>
+                    <div class="value">
+                        
+                    </div>
+                </a-col>
+                <a-col class="search-col">
+                    <div class="key">
+                        定时发送
+                    </div>
+                    <div class="value">
+                        
+                    </div>
+                </a-col>
+            </a-row>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+
+const formData = ref({
+
+})
+
+</script>
+
+<style lang="less" scoped>
+.add-mail {
+    .container-block {
+    }
+}
+</style>
---
+++ src/views/crm-mail-management/mail-send-statistics.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="mail-send-statistics">
+        <a-button @click="btn">新增邮件</a-button>
+    </div>
+</template>
+
+<script setup>
+import { getCurrentInstance, ref } from 'vue';
+import { useRouter } from 'vue-router'
+const { proxy } = getCurrentInstance()
+
+
+const router = useRouter()
+const btn = () => {
+    console.log("新增邮件");
+    router.push('/mail-management/add-mail')
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
