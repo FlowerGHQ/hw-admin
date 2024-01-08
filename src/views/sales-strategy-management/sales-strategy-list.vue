f12098d5653cf90543e3bf534acb8e25bff7bc4e
dzy <dzyperson@163.com>
2024-01-08 11:42:39 +0800

    fix：修改

--- src/views/sales-strategy-management/sales-strategy-list.vue
+++ src/views/sales-strategy-management/sales-strategy-list.vue
@@ -4,7 +4,8 @@
             <SearchAll
                 :options="searchList"
                 @search="getSearchFrom"
-                @reset="handleSearchReset" />
+                @reset="handleSearchReset"
+                :isShowMore="false" />
         </div>
         <div class="addd-btn">
             <el-button type="primary" icon="el-icon-plus" @click="add"
@@ -20,7 +21,28 @@ import SearchAll from "@/components/common/SearchAll.vue";
 import { ref } from "vue";
 
 const searchList = ref([
-	{ type: "input", value: "", searchParmas: "name",  key: 'n.name' },// 名称
+    {
+        id: 0,
+        type: "input",
+        value: "",
+        searchParmas: "strategy_name",
+        key: "sales-strategy.strategy_name",
+    }, // 名称
+    {
+        id: 2, // 随意
+        type: "select", // 类型
+        key: "sales-strategy.area", // 名称
+        value: undefined, // 绑定值
+        searchParmas: "area", // 返回的搜索名称
+        selectMap: [
+            {
+                zh: "整车",
+                en: "Vehicle",
+                value: 1, // a-select-option 中的 :value="item.value"  // 固定的
+            },
+        ],
+        placeholder: "def.select",
+    },
 ]);
 </script>
 
