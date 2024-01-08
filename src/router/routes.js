407adbf432572242a85ca43a70284dda12bd7107
罗轶 <luoyi54666@163.com>
2024-01-08 11:27:03 +0800

    feat:分销商登录改回旧

--- src/router/routes.js
+++ src/router/routes.js
@@ -16,13 +16,13 @@ switch (NOW_LOGIN_TYPE) {
         indexPath = '/distributor'
         break;
     case LOGIN_TYPE.DISTRIBUTOR:
-        indexPath = '/dashboard/index'
+        indexPath = '/mall/index'
         break;
     case LOGIN_TYPE.AGENT:
-        indexPath = '/dashboard/index'
+        indexPath = '/mall/index'
         break;
     case LOGIN_TYPE.STORE:
-        indexPath = '/dashboard/index'
+        indexPath = '/mall/index'
         break;
 
     default:
@@ -52,15 +52,15 @@ const routes = [
 	        title_en: 'login-redirect',
         }
     },
-    // { // 登录
-    //     path: '/login',
-    //     component: () => import ('@/views/Login.vue'),
-    //     meta: {
-    //         hidden: true,
-    //         title: '登录',
-	//         title_en: 'Login',
-    //     }
-    // },
+    { // 登录
+        path: '/login',
+        component: () => import ('@/views/Login.vue'),
+        meta: {
+            hidden: true,
+            title: '登录',
+	        title_en: 'Login',
+        }
+    },
     { // 分销商登录
         path: '/login',
         component: () => import ('@/views/mall/Login.vue'),
