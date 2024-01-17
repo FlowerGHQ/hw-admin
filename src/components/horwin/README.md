7965b152df8becfea57026bdde0f428806cae4b6
15779969509 <ten_ann@163.com>
2024-01-17 17:18:11 +0800

    fix: 添加自定义组件文件

---
+++ src/components/horwin/README.md
@@ -0,0 +1,11 @@
+## 概述
+
+```js
+文件夹
+	horwin
+      - based-on-ant  // 基于"ant-design-vue": "3.1.1", 二次封装组件
+	  - based-on-dom  // 原生DOM写的
+```
+
+
+
---
+++ src/components/horwin/based-on-dom/MyMask.vue
@@ -0,0 +1,40 @@
+<template>
+    <!-- 遮罩层 -->
+    <view v-if="isClose" class="page-container mask" @click="onMask">        
+        <slot name="centers"></slot>
+    </view>
+</template>
+
+<script>
+    // vue需要 2.7.0 以上的版本
+    export default {
+        props: {
+            isClose: {
+                type: Boolean,
+                default: true
+            }
+        },
+        setup(props, { emit }){
+            // 关闭这个
+            const onMask = () => {    
+                emit("close", false)
+            }
+
+            return {
+                onMask
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.mask {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: rgba(0,0,0,0.5);
+    z-index: 999999;
+}
+</style>
