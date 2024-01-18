8ce4a4d496a65aed9a194c583ac91c4d5282e2f8
dzy <dzyperson@163.com>
2024-01-18 10:18:15 +0800

    style:样式修改

--- src/views/sales-strategy-management/components/ClassifyModal.vue
+++ src/views/sales-strategy-management/components/ClassifyModal.vue
@@ -1,126 +1,103 @@
 <template>
-    <a-modal
-        v-model:visible="visibility"
-        :getContainer="handleGetContainer"
-        :width="860"
-        title="添加明细"
-        @ok="handleOk"
-        @cancel="handleCancle">
-        <!-- 搜索固定数据 :closable="false" -->
-        <div class="search-container-new top-box">
-            <div class="item-box">
-                <div class="key-box">选择赠送地区</div>
-                <div class="value-box">
-                    <a-select
-                        v-model:value="searchForm.area"
-                        :options="CountryData"
-                        mode="multiple"
-                        show-search
-                        :filter-option="filterOption"
-                        :placeholder="$t('def.select')">
-                        <template #removeIcon>
-                            <MySvgIcon icon-class="sales-circle-delete" />
-                        </template>
-                    </a-select>
-                </div>
-            </div>
-        </div>
-        <div class="search">
-            <SearchAll
-                :isShowMore="false"
-                :disabled="level === 2"
-                @search="handleSearch"
-                @reset="handleSearchReset">
-                <template v-slot:extend>
-                    <a-col
-                        v-if="options.type === 'input'"
-                        :xs="24"
-                        :sm="15"
-                        :xl="15"
-                        :xxl="15"
-                        class="search-box">
-                        <div class="item-box">
-                            <div class="key-box">
-                                {{ $t(options.key) }}
-                            </div>
-                            <div class="value-box">
-                                <a-input
-                                    :disabled="level === 2"
-                                    :placeholder="
-                                        $t(
-                                            `${
-                                                options.placeholder ||
-                                                'def.input'
-                                            }`
-                                        )
-                                    "
-                                    v-model:value="codeStr"
-                                    @keydown.enter="handleSearch" />
-                            </div>
-                        </div>
-                    </a-col>
-                </template>
-            </SearchAll>
-        </div>
-        <!-- 占位盒子 -->
-        <div class="fixed"></div>
-        <div style="height: 140px"></div>
-        <!-- 表格组件 -->
-        <TableSelect
-            ref="childRef"
-            :columns="tableColumns"
-            :data-source="tableData"
-            :loading="loading"
-            :check-mode="true"
-            @submit="getSelectIdList">
-        </TableSelect>
-        <template #footer>
-            <div class="modal-footer">
-                <div class="paging-area">
-                    <a-pagination
-                        v-model:current="current"
-                        v-model:pageSize="pageSize"
-                        show-size-changer
-                        :total="total"
-                        @change="pageChange"
-                        @showSizeChange="onShowSizeChange" />
-                    <div class="tip">
-                        {{
-                            $t("in.selected") +
-                            ` ${selectIdList.length} ` +
-                            $t("in.total")
-                        }}
+    <div ref="modalContentArea">
+        <a-modal
+            v-model:visible="visibility"
+            :getContainer="handleGetContainer"
+            :width="860"
+            title="添加明细"
+            @ok="handleOk"
+            @cancel="handleCancle">
+            <!-- 搜索固定数据 :closable="false" -->
+            <div class="search-container-new">
+                <div class="item-box">
+                    <div class="key-box">选择赠送地区</div>
+                    <div class="value-box">
+                        <a-select
+                            v-model:value="searchForm.area"
+                            :options="CountryData"
+                            mode="multiple"
+                            show-search
+                            :filter-option="filterOption"
+                            :placeholder="$t('def.select')">
+                            <template #removeIcon>
+                                <MySvgIcon icon-class="sales-circle-delete" />
+                            </template>
+                        </a-select>
                     </div>
                 </div>
-                <div class="btn-area">
-                    <a-button @click="handleCancle">{{
-                        $t("def.cancel")
-                    }}</a-button>
-                    <a-button @click="handleOk" type="primary">{{
-                        $t("def.sure")
-                    }}</a-button>
-                </div>
             </div>
-        </template>
-    </a-modal>
+            <div class="search">
+                <SearchAll
+                    :isShowMore="false"
+                    :disabled="level === 2"
+                    @search="handleSearch"
+                    @reset="handleSearchReset">
+                    <template v-slot:extend>
+                        <a-col v-if="options.type === 'input'" :xs="24" :sm="15" :xl="15" :xxl="15" class="search-box">
+                            <div class="item-box">
+                                <div class="key-box">
+                                    {{ $t(options.key) }}
+                                </div>
+                                <div class="value-box">
+                                    <a-input
+                                        :disabled="level === 2"
+                                        :placeholder="$t(`${options.placeholder || 'def.input'}`)"
+                                        v-model:value="codeStr"
+                                        @keydown.enter="handleSearch" />
+                                </div>
+                            </div>
+                        </a-col>
+                    </template>
+                </SearchAll>
+            </div>
+            <TableSelect
+                ref="childRef"
+                :columns="tableColumns"
+                :data-source="tableData"
+                :loading="loading"
+                :check-mode="true"
+                @submit="getSelectIdList">
+            </TableSelect>
+            <template #footer>
+                <div class="modal-footer">
+                    <div class="paging-area">
+                        <a-pagination
+                            v-model:current="current"
+                            v-model:pageSize="pageSize"
+                            show-size-changer
+                            :total="total"
+                            @change="pageChange"
+                            @showSizeChange="onShowSizeChange" />
+                        <div class="tip">
+                            {{ $t("in.selected") + ` ${selectIdList.length} ` + $t("in.total") }}
+                        </div>
+                    </div>
+                    <div class="btn-area">
+                        <a-button @click="handleCancle">{{ $t("def.cancel") }}</a-button>
+                        <a-button @click="handleOk" type="primary">{{ $t("def.sure") }}</a-button>
+                    </div>
+                </div>
+            </template>
+        </a-modal>
+    </div>
 </template>
 
 <script setup>
 import { onMounted, ref, getCurrentInstance, computed, watch } from "vue";
 import Core from "@/core";
 import SearchAll from "@/components/common/SearchAll.vue";
-import TableSelect from './ItemTale.vue';
+import TableSelect from "./ItemTale.vue";
 import MySvgIcon from "@/components/MySvgIcon/index.vue";
 import COUNTYR from "@/assets/js/address/countries.json";
-import {message} from "ant-design-vue";
+import { message } from "ant-design-vue";
 import { useI18n } from "vue-i18n";
-import {useRoute} from "vue-router"; 
+import { useRoute } from "vue-router";
 import _ from "lodash";
 const { proxy } = getCurrentInstance();
 const $i18n = useI18n();
 const $t = $i18n.t;
 const $route = useRoute();
-const emits = defineEmits(["update:visibility",'hanldAdd','hanldItemList','handleEdit']);
+const emits = defineEmits(["update:visibility", "hanldAdd", "hanldItemList", "handleEdit"]);
 const props = defineProps({
     visibility: {
         type: Boolean,
@@ -144,15 +121,16 @@ const CountryData = computed(() => {
 const initialObject = {
     // 商品编码
     codeList: [],
-    area:[]
+    area: [],
 };
 const childRef = ref(null);
+const modalContentArea = ref(null);
 // 当前分组对象
 // const classValue =  ref();
 const searchForm = ref({
     // 商品编码
     codeList: [],
-    area:[]
+    area: [],
 });
 // 分类列表
 // 搜索列表组件
@@ -184,7 +162,7 @@ const pageChange = () => {
     getTableDataFetch();
 };
 const handleGetContainer = () => {
-    return document.querySelector(".sales-strategy-edit")
+    return modalContentArea.value;
 };
 const loading = ref(false);
 const tableColumns = computed(() => {
@@ -197,9 +175,7 @@ const tableColumns = computed(() => {
         },
         {
             // 分类
-            title: proxy.$t(
-                "sales-strategy-management.commodity_classification"
-            ),
+            title: proxy.$t("sales-strategy-management.commodity_classification"),
             dataIndex: "category_list",
             key: "category_list",
         },
@@ -249,11 +225,11 @@ const addTableData = () => {
 const handleOk = () => {
     // 判断是否选择了地区和商品
     if (searchForm.value.area.length === 0) {
-        message.warning($t("sales-strategy-management.please_select_the_gift_area"))
+        message.warning($t("sales-strategy-management.please_select_the_gift_area"));
         return;
     }
     if (selectIdList.value.length === 0) {
-        message.warning($t("sales-strategy-management.please_select_the_gift_item"))
+        message.warning($t("sales-strategy-management.please_select_the_gift_item"));
         return;
     }
     // 从本地取出数据
@@ -261,43 +237,40 @@ const handleOk = () => {
     let maxNo = 0;
     if (arr.length === 0) {
         maxNo = 0;
-    } 
-    else if (arr.length === 1) {
+    } else if (arr.length === 1) {
         maxNo = arr[0].no;
-    }
-    else {
-       maxNo = arr.reduce((prev, cur) => {
-        // 迭代器，找出最大的no
+    } else {
+        maxNo = arr.reduce((prev, cur) => {
+            // 迭代器，找出最大的no
             return prev.no > cur.no ? prev.no : cur.no;
         });
     }
-    console.log('maxNo',maxNo)
-    let strategy_detail = []
+    console.log("maxNo", maxNo);
+    let strategy_detail = [];
 
     selectIdList.value.forEach((item) => {
         searchForm.value.area.forEach((item1) => {
-            let obj =  {}
-            obj.no = maxNo + 1 ;
+            let obj = {};
+            obj.no = maxNo + 1;
             obj.country = item1;
             obj.bonus_item_id = item;
             obj.item = selectItemList.value.filter((item2) => item2.id === item)[0];
-            if($route.query.type === 'add' ){
-            //    赋值唯一id
-                obj.id = _.uniqueId();  
+            if ($route.query.type === "add") {
+                //    赋值唯一id
+                obj.id = _.uniqueId();
             }
-            if(isEdit.value){
+            if (isEdit.value) {
                 obj.isEdit = true;
             }
             strategy_detail.push(obj);
-
         });
     });
-    if(isEdit.value){
-        emits('handleEdit',strategy_detail)
+    if (isEdit.value) {
+        emits("handleEdit", strategy_detail);
         isEdit.value = false;
-    }else{
-        emits('hanldItemList',selectIdList.value)
-        emits('hanldAdd',strategy_detail)
+    } else {
+        emits("hanldItemList", selectIdList.value);
+        emits("hanldAdd", strategy_detail);
     }
     handleCancle();
 };
@@ -314,7 +287,7 @@ const getTableDataFetch = (parmas = {}) => {
         flag_spread: 1,
         page: current.value,
         page_size: pageSize.value,
-        status:0,
+        status: 0,
         ...parmas,
     };
     Core.Api.Item.list(obj)
@@ -330,7 +303,7 @@ const getTableDataFetch = (parmas = {}) => {
         });
 };
 // 接收选择id的数组
-const getSelectIdList = (kesArr,itemsArr) => {
+const getSelectIdList = (kesArr, itemsArr) => {
     selectIdList.value = Core.Util.deepCopy(kesArr);
     selectItemList.value = Core.Util.deepCopy(itemsArr);
 };
@@ -340,27 +313,29 @@ watch(
     (newValue, oldValue) => {
         if (newValue) {
             getTableDataFetch();
-        }else{
+        } else {
             // 清空数据
             selectIdList.value = [];
         }
     }
 );
-watch(()=>props.reviewData,
-    (newValue,oldValue)=>{
-    if(newValue){
-       console.log('newValue',newValue)
-       searchForm.value.area = [newValue.country ]
-       searchForm.value.codeList = newValue.item.map((item)=>item.code)
-       codeStr.value = newValue.item.map((item)=>item.code).join(',')
-       selectIdList.value = newValue.item.map((item)=>item.id)
-       isEdit.value = true;
-       handleSearch();
-       setTimeout(() => {
-        childRef.value.selectedRowKeys = newValue.item.map((item)=>item.id)
-       }, 1000);
+watch(
+    () => props.reviewData,
+    (newValue, oldValue) => {
+        if (newValue) {
+            console.log("newValue", newValue);
+            searchForm.value.area = [newValue.country];
+            searchForm.value.codeList = newValue.item.map((item) => item.code);
+            codeStr.value = newValue.item.map((item) => item.code).join(",");
+            selectIdList.value = newValue.item.map((item) => item.id);
+            isEdit.value = true;
+            handleSearch();
+            setTimeout(() => {
+                childRef.value.selectedRowKeys = newValue.item.map((item) => item.id);
+            }, 1000);
+        }
     }
-})
+);
 
 onMounted(() => {
     // 初始化数据
@@ -369,36 +344,26 @@ onMounted(() => {
 </script>
 
 <style lang="less" scoped>
-.search {
-    position: absolute;
-    top: 120px;
-    padding: 0;
-    width: -moz-calc(100% - 48px);
-    width: -webkit-calc(100% - 48px);
-    width: calc(100% - 48px);
-    z-index: 20;
+.modal-content-area{
+
+    .tip {
+        height: 30px;
+        line-height: 30px;
+        margin-left: 10px;
+        font-size: 12px;
+    }
+   
 }
-.top-box {
-    position: absolute;
-    z-index: 20;
-    min-width: 810px;
-}
-.tip {
-    height: 30px;
-    line-height: 30px;
-    margin-left: 10px;
-    font-size: 12px;
-}
-.fixed {
-    background-color: #ffffff;
-    height: 140px;
-    position: absolute;
-    top: 44px;
-    width: -moz-calc(100% - 24px);
-    width: -webkit-calc(100% - 24px);
-    width: calc(100% - 24px);
-    left: 0;
-    z-index: 0;
-    z-index: 15;
-}
+:deep(.ant-modal-body){
+    display: flex;
+    flex-direction: column;
+        .search {
+            padding: 0 !important;
+        }
+        .ant-table-wrapper{
+            flex: 1;
+            margin-top: 20px;
+            overflow: auto;
+        }
+    }
 </style>
