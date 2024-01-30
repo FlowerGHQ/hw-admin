9f94cb1ad9dcf6e5c2b88b92c159699fc7c69871
dzy <dzyperson@163.com>
2024-01-30 13:59:24 +0800

    style:修改样式

--- src/views/item/item-edit.vue
+++ src/views/item/item-edit.vue
@@ -3421,7 +3421,6 @@ export default {
 
 .ant-input-group-wrapper {
     :deep(.ant-input-group-addon) {
-        border-color: #eaecf2 !important;
         color: #8090A6;
         .l-w-h-style-unit{
             color: #8090A6;
