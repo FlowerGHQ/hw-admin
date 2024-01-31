2f3b949d6e2993c4dcc9c7b5583e8ff0f362f32e
yixinyu <498065027@qq.com>
2024-01-31 16:52:08 +0800

    feat: report table

---
+++ src/core/modules/const/operation/index.js
@@ -0,0 +1,10 @@
+const OPERATION = {
+    OPERATION_TYPE: {
+        '0': { value: 0, zh: '全部', en: 'All' },
+        '1': { value: 1, zh: '已发布', en: 'Have Released' },
+        '2': { value: 2, zh: '未发布', en: 'Unpublish' },
+    },
+}
+
+export default OPERATION
+
