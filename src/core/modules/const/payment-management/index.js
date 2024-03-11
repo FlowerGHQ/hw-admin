eea9cef7aaf13f9d093aebd9543266f7b51f579e
yixinyu <498065027@qq.com>
2024-03-11 14:27:02 +0800

    feat: payment management list

---
+++ src/core/modules/const/payment-management/index.js
@@ -0,0 +1,13 @@
+const PAYMENT_MANAGEMENT = {
+    EFFECTIVE_TYPE: {
+        0: { value: 0, zh: '全部', en: 'All' },
+        1: { value: 1, zh: '已生效', en: 'Took Effect' },
+        2: { value: 2, zh: '未生效', en: 'Not Activated' },
+    },
+    EFFECTIVE_TYPE_MAP: {
+        YES: 1, // 已生效
+        NO: 2, // 未生效
+    },
+};
+
+export default PAYMENT_MANAGEMENT;
