1ec71bfd35c848d4f1f404534d3ca95155aa761e
罗轶 <luoyi54666@163.com>
2024-03-15 14:54:41 +0800

    feat:充值页

--- src/views/mall/order/recharge.vue
+++ src/views/mall/order/recharge.vue
@@ -157,6 +157,7 @@ import { useRoute, useRouter } from 'vue-router';
 
 const { proxy } = getCurrentInstance();
 
+const route = useRoute();
 const router = useRouter();
 const id = ref('');
 const orgId = ref(route.query?.id || Core.Data.getOrgId()); // 分销商id
