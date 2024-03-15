d43d732d4f59aaace102314c6f3c5e9ff31aa6c4
dzy <dzyperson@163.com>
2024-03-15 21:40:35 +0800

    fix:授信余额的修改

--- src/views/distributor/components/AccountWallet.vue
+++ src/views/distributor/components/AccountWallet.vue
@@ -342,7 +342,7 @@ const getWalletList = () => {
         for (let i = 0; i < res.list.length; i++) {
             let item = res.list[i];
             let key = typeMap[item.type];
-            item.balance = Core.Util.countFilter(item?.balance || 0) * 1;
+            item.balance = Core.Util.countFilter(item?.balance || 0, 100, 2, false, true) * 1;
             dataObject.value[key] = item;
         }
         console.log('dataObject', dataObject.value);
@@ -428,7 +428,8 @@ watch(
     () => props.detail,
     (newV, oldV) => {
         console.log('newV', newV);
-        creditBalance.value = Core.Util.countFilter(newV.credit) * 1 + dataObject.value.creditData.balance * 1;
+        creditBalance.value =
+            Core.Util.countFilter(newV.credit, 100, 2, false, true) * 1 + dataObject.value.creditData.balance * 1;
     },
 );
 
