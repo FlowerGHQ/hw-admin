8d2e8f811111b201ba979afd91dbae093cd809ba
15779969509 <ten_ann@163.com>
2024-03-19 10:57:40 +0800

    fix: 修改延期时间

--- src/views/distributor/final-unpaid-payment/list.vue
+++ src/views/distributor/final-unpaid-payment/list.vue
@@ -463,6 +463,7 @@ const onTabChange = type => {
             break;
         case FINAL_UNPAID_ORDER_TAB.DELAY:
             searchForm.value.final_pay_due_begin_time = dayjs().unix();
+            searchForm.value.final_pay_due_end_time = dayjs().add(7, 'day').unix();
             break;
     }
     searchParam.value = Core.Util.searchFilter(searchForm.value);
