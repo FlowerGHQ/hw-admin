8444bcd862d2869138cb1935ee096a56fd88b0b7
15779969509 <ten_ann@163.com>
2024-03-26 21:38:49 +0800

    fix: 修改上传域名

--- src/core/const.js
+++ src/core/const.js
@@ -49,8 +49,8 @@ switch (window.location.hostname) {
         break;
     default:
         // URL_POINT = 'https://eos-dev-api.horwincloud.com'; // 测试服
-        URL_POINT = 'https://eos-api.horwincloud.com'; // 新正式服
-        // URL_POINT = 'https://eos-api-release.horwincloud.com'; // 预发环境
+        // URL_POINT = 'https://eos-api.horwincloud.com'; // 新正式服
+        URL_POINT = 'https://eos-api-release.horwincloud.com'; // 预发环境
         // URL_POINT = 'http://10.0.0.170:8889' // my
         // URL_POINT = 'http://10.10.12.194:8889' // zwq
         // URL_POINT = 'http://10.10.12.75:8889'  // zy
